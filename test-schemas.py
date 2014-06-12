7df7ad54e3e3ee31fdd38ca6d1afa93a72f1c554
Tim Bielawa <tbielawa@redhat.com>
2014-06-12 14:07:25 -0400

    Scaffolding for test documents

--- test-schemas.py
+++ test-schemas.py
@@ -14,10 +14,11 @@ schemas = os.listdir(schema_dir)
 test_docs = os.listdir(test_dir)
 
 schema_validation_results = {}
+document_validation_results = {}
 test_map = {}
 
 for test_doc in test_docs:
-    print "Trying to find a schema for %s" % test_doc
+    # print "Trying to find a schema for %s" % test_doc
 
     for schema in schemas:
         schema_basename = schema.replace('.json', '')
@@ -31,7 +32,7 @@ for test_doc in test_docs:
 
 def validate_schemas():
     for schema in schemas:
-        print ">>> Validating schema: %s" % schema
+        # print ">>> Validating schema: %s" % schema
         valid = False
         try:
             with open(os.path.join(schema_dir, schema), 'r') as schema_fp:
@@ -39,25 +40,43 @@ def validate_schemas():
             Draft4Validator.check_schema(s)
         except Exception, e:
             print ">>> %s: FAILED" % schema
+
             print e
+            print ""
         else:
-            print ">>> %s: PASSED" % schema
+            #print ">>> %s: PASSED" % schema
             valid = True
 
         schema_validation_results[schema] = valid
-        print """
-"""
 
-
+    print "Schema Validity:"
     pp(schema_validation_results)
 
 
-# for doc, schema in test_map.iteritems():
+def validate_test_documents():
+    for doc, schema in test_map.iteritems():
+        print "Validating document %s using %s schema" % (doc, schema)
 
-print """
+        valid = False
+        try:
+            with open(os.path.join(test_dir, doc), 'r') as test_doc_fp:
+                test_doc = json.loads(test_doc_fp.read())
+
+            with open(os.path.join(schema_dir, schema), 'r') as test_schema_fp:
+                test_schema = json.loads(test_schema_fp.read())
+
+            jsonschema.validate(test_doc, test_schema)
+        except Exception, e:
+            print e
+        else:
+            valid = True
+
+        document_validation_results[doc] = valid
+
+    print "Document Validity:"
+    pp(document_validation_results)
 
 
-
-
-"""
 validate_schemas()
+print ""
+validate_test_documents()
